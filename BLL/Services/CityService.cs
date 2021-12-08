2345bf4605e7d8db9c91205362cad671a3c3b450
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-12-08 16:29:30 +0300

    add service

---
+++ BLL/Services/CityService.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.EF;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CityService : ICityService
+    {
+        private readonly DataContext db;
+        private bool disposed = false;
+        private IMapper Mapper => new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<City, CityDTO>().ReverseMap();
+            cfg.CreateMap<Order, OrderDTO>();
+        }).CreateMapper();
+
+        public CityService(DataContext db) => this.db = db;
+
+        public IEnumerable<CityDTO> GetAll() => Mapper.Map<IEnumerable<City>, List<CityDTO>>(db.Cities);
+        public CityDTO Get(Guid id) => Mapper.Map<City, CityDTO>(db.Cities.Find(id));
+        public void Create(CityDTO item)
+        {
+            if (db.Cities.Find(item.Id) == null)
+                db.Cities.Add(Mapper.Map<CityDTO, City>(item));
+        }
+        public void Update(CityDTO item)
+        {
+            var city = db.Cities.Find(item.Id);
+            if (city != null)
+            {
+                city.Name = item.Name;
+                db.Cities.Update(city);
+            }
+        }
+        public void Delete(Guid id)
+        {
+            var city = db.Cities.Find(id);
+            if (city != null)
+                db.Cities.Remove(city);
+        }
+        public void Save() => db.SaveChanges();
+        public async Task<IEnumerable<CityDTO>> GetAllAsync() => Mapper.Map<IEnumerable<City>, List<CityDTO>>(await db.Cities.ToListAsync());
+        public async Task<CityDTO> GetAsync(Guid id) => Mapper.Map<City, CityDTO>(await db.Cities.FindAsync(id));
+        public async Task CreateAsync(CityDTO item)
+        {
+            if (db.Cities.Find(item.Id) == null)
+            {
+                db.Cities.Add(Mapper.Map<CityDTO, City>(item));
+                await db.SaveChangesAsync();
+            }
+        }
+        public async Task UpdateAsync(CityDTO item)
+        {
+            var city = await db.Cities.FindAsync(item.Id);
+            if (city != null)
+            {
+                city.Name = item.Name;
+                db.Cities.Update(city);
+                await db.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var city = await db.Cities.FindAsync(id);
+            if (city != null)
+            {
+                db.Cities.Remove(city);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                db.Dispose();
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ BLL/Services/OrderService.cs
@@ -0,0 +1,125 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.EF;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly DataContext db;
+        private bool disposed = false;
+        private IMapper Mapper => new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Order, OrderDTO>().ReverseMap();
+            cfg.CreateMap<City, CityDTO>();
+        }).CreateMapper();
+
+        public OrderService(DataContext db) => this.db = db;
+
+        public IEnumerable<OrderDTO> GetAll() => Mapper.Map<IEnumerable<Order>, List<OrderDTO>>(db.Orders.Include(x => x.City));
+        public OrderDTO Get(Guid id) => Mapper.Map<Order, OrderDTO>(db.Orders.Include(x => x.City).FirstOrDefault(x => x.Id == id));
+        public void Create(OrderDTO item)
+        {
+            if (db.Orders.Find(item.Id) == null)
+            {
+                item.Price = CalcPrice(item);
+                db.Orders.Add(Mapper.Map<OrderDTO, Order>(item));
+            }
+        }
+        public void Update(OrderDTO item)
+        {
+            var order = db.Orders.Find(item.Id);
+            if (order != null)
+            {
+                item.Price = CalcPrice(item);
+                order.FirstName = item.FirstName;
+                order.LastName = item.LastName;
+                order.Weight = item.Weight;
+                order.CityId = item.CityId;
+                order.SendType = item.SendType;
+                order.Comment = item.Comment;
+                order.Price = item.Price;
+                db.Orders.Update(order);
+            }
+        }
+        public void Delete(Guid id)
+        {
+            var order = db.Orders.Find(id);
+            if (order != null)
+                db.Orders.Remove(order);
+        }
+        public void Save() => db.SaveChanges();
+        public async Task<IEnumerable<OrderDTO>> GetAllAsync() => Mapper.Map<IEnumerable<Order>, List<OrderDTO>>(await db.Orders.Include(x => x.City).ToListAsync());
+        public async Task<OrderDTO> GetAsync(Guid id) => Mapper.Map<Order, OrderDTO>(await db.Orders.Include(x => x.City).FirstOrDefaultAsync(x => x.Id == id));
+        public async Task CreateAsync(OrderDTO item)
+        {
+            if (db.Orders.Find(item.Id) == null)
+            {
+                item.Price = CalcPrice(item);
+                db.Orders.Add(Mapper.Map<OrderDTO, Order>(item));
+                await db.SaveChangesAsync();
+            }
+        }
+        public async Task UpdateAsync(OrderDTO item)
+        {
+            var order = await db.Orders.FindAsync(item.Id);
+            if (order != null)
+            {
+                order.FirstName = item.FirstName;
+                order.LastName = item.LastName;
+                order.Weight = item.Weight;
+                order.CityId = item.CityId;
+                order.SendType = item.SendType;
+                order.Comment = item.Comment;
+                order.Price = CalcPrice(item);
+                db.Orders.Update(order);
+                await db.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var order = await db.Orders.FindAsync(id);
+            if (order != null)
+            {
+                db.Orders.Remove(order);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        private decimal CalcPrice(OrderDTO order)
+        {
+            var city = db.Cities.Find(order.CityId).Name;
+            decimal costPerKg = 30;
+            decimal deliveryCostToCity = 0;
+            switch (city)
+            {
+                case "Киев":
+                    deliveryCostToCity = 60;
+                    break;
+                case "Харьков":
+                    deliveryCostToCity = 40;
+                    break;
+                case "Полтава":
+                    deliveryCostToCity = 20;
+                    break;
+            }
+            return deliveryCostToCity + (decimal)order.Weight * costPerKg;
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                db.Dispose();
+                disposed = true;
+            }
+        }
+    }
+}
