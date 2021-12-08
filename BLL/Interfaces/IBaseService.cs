b20c628c0ff08d94c12377d636ce88e9fc418ea3
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-12-08 16:29:20 +0300

    add service interfaces

---
+++ BLL/Interfaces/IBaseService.cs
@@ -0,0 +1,23 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IBaseService<T> : IDisposable
+         where T: BaseModelDTO
+    {
+        Task<T> GetAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task UpdateAsync(T item);
+        Task CreateAsync(T item);
+        Task DeleteAsync(Guid id);
+
+        T Get(Guid id);
+        IEnumerable<T> GetAll();
+        void Update(T item);
+        void Create(T item);
+        void Delete(Guid id);
+    }
+}
---
+++ BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿using BLL.DTO;
+
+namespace BLL.Interfaces
+{
+    public interface IOrderService : IBaseService<OrderDTO>
+    {
+    }
+}
