aadcb2b145ee1da280af0ea6894a10f1248cb67b
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-12-08 16:28:06 +0300

    add database and data

---
+++ DAL/EF/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ DAL/EF/DataSeed.cs
@@ -0,0 +1,72 @@
+﻿using DAL.Entities;
+using Domain.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class DataSeed
+    {
+        public static async Task Seed(DataContext db)
+        {
+            if (!db.Orders.Any())
+            {
+                var cities = new List<City>
+                {
+                    new City
+                    {
+                        Name = "Киев"
+                    },
+                    new City
+                    {
+                        Name = "Харьков"
+                    },
+                    new City
+                    {
+                        Name = "Полтава"
+                    }
+                };
+
+                foreach (var item in cities)
+                    await db.Cities.AddAsync(item);
+
+                var orders = new List<Order>
+                {
+                    new Order
+                    {
+                        FirstName ="Руслан",
+                        LastName ="Демитов",
+                        CityId = cities[0].Id,
+                        Comment ="",
+                        Weight = 68.5,
+                        SendType = SendType.OversizedCargo
+                    },
+                    new Order
+                    {
+                        FirstName ="Владимир",
+                        LastName ="Кузнецов",
+                        CityId = cities[1].Id,
+                        Comment = "не очень длинынй комментарий",
+                        Weight = 8.5,
+                        SendType = SendType.OverallCargo
+                    },
+                    new Order
+                    {
+                        FirstName ="Евгений",
+                        LastName ="Шаповалов",
+                        CityId = cities[2].Id,
+                        Comment = "какой то очень длинный комментарий",
+                        Weight = 0.1,
+                        SendType = SendType.Letter
+                    }
+                };
+
+                foreach (var item in orders)
+                    await db.Orders.AddAsync(item);
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
